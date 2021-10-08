fd42f7258f448700e459ce4d03d824539e217c5c
vantage-ola <vantageola4@gmail.com>
2021-10-08 11:40:11 +0100

    Add: imports, initialstate

--- src/hooks/useLyricFetch.js
+++ src/hooks/useLyricFetch.js
@@ -2,30 +2,42 @@ import { useState, useEffect } from 'react';
 
 import API from '../API';
 
+//Test Data
+import Test from '../test/Test';
+
+const initialState = {
+	song: null
+};
+
 export const useLyricFetch = () => {
 
 
 	const [searchTerm, setSearchTerm] = useState('');
 
-	const [state, setState] = useState([]);
+	const [state, setState] = useState(initialState);
+	const [loading, setLoading] = useState(false);
+	const [isLoadingMore, setIsLoadingMore] = useState(false);
 	const [error, setError] = useState(false);
 
 	const fetchArtistSongs = async(id , page) => {
 		try {
 
 			setError(false);
+			setLoading(true);
 
-			const songs = await API.fetchArtistSongs(id, page);
-
-			setState(songs)
+			const data = await API.fetchArtistSongs(id, page);
+			//Test Data
+			//const data = Test;
+			setState({ song: data.response.songs});
 
 		} catch(error) {
 			setError(true);
 		}
+		setLoading(false);
 	};
 	useEffect(() => {
 		fetchArtistSongs(1123, 1);
 	}, []);
 
-	return {state}
+	return {state, loading, error};
 };
