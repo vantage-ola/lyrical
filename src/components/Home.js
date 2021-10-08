aeb81adb96258e7c5f36424bdc6f205c29e3bfb4
vantage-ola <vantageola4@gmail.com>
2021-10-08 11:41:01 +0100

    Add: imports, render grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,27 @@
 import { Footer } from "./Footer";
 import Header from "./Header";
 
-import styled from "styled-components";
+import { useLyricFetch } from "../hooks/useLyricFetch";
+import { LoadSpinner } from "./LoadSpinner/LoadSpinner.styles";
+import Grid from "./Grid";
 
 const Home = () => {
 
+    const {state} = useLyricFetch();
+    console.log(state)
 
     return (
         <>
+
         <Header/>
+        {state.song ? (
+                    <Grid header='Songs'>
+                    {state.song.map(songs => (
+                        <div key={songs.id}>{songs.title}</div>
+                    ))}
+                </Grid>
+        ): null}
+        <LoadSpinner/>
         <Footer/>
         </>
     )
