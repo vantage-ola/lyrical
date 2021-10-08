20a71db459494e19b29a0cbdaa1adcb4e39568f9
vantage-ola <vantageola4@gmail.com>
2021-10-08 11:34:41 +0100

    Removed unnessary imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-
-import {useLyricFetch} from './hooks/useLyricFetch';
-
 import Home from './components/Home';
 
 import { DefaultStyle } from './DefaultStyle';
 
 function App() {
 
-  const {state} = useLyricFetch();
-      
-//  console.log(state)
   return (
     <>
     <DefaultStyle/>
