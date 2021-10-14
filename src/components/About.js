65051aed028ccbb62e15ad1a40f435718b9a0c79
vantage-ola <vantageola4@gmail.com>
2021-10-14 06:45:35 +0100

    Add: Added little info to About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,42 @@
 import React from "react";
 
+import styled from "styled-components";
+
+import {Footer} from './Footer'
+
+export const Wrapper = styled.div`
+  max-width: 1660px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  h1 {
+    color: var(--white);
+
+    @media screen and (max-width: 768px) {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+export const Content = styled.div`
+    grid-column: 2 / span 12;
+    min-height: 3rem;
+    padding: 3rem 0;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+	`;
+
 const About = () => {
 
 	return (
 		<>
-		<p>About</p>
+		<Wrapper>
+			<h1>About</h1>
+			<Content>
+			<h3>Lyrical is a third party platform that uses the Genius api to display information about music. </h3>
+			</Content>
+		</Wrapper>
 		</>
 	);
 };
