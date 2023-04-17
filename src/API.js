ac48c36d9af4e026d28dff3dd8c7fd99d84c40f7
vantage-ola <vantageola4@gmail.com>
2023-04-17 11:05:15 +0100

    + access token + changed api url

--- src/API.js
+++ src/API.js
@@ -5,30 +5,24 @@ import {
 } from './config'
 
 
-const defaultConfig = {
- method: 'GET',
- "headers": {
-		"x-rapidapi-host": "genius.p.rapidapi.com",
-		"x-rapidapi-key": `${API_KEY}`,
-}, 
-};
+
 
 const API = {
 
  fetchArtistSongs: async( id, page) => {
-  const endpoint = `${API_URL}artists/${id}/songs?sort=popularity&page=${page}&per_page=48`;
-  return await (await fetch(endpoint, {...defaultConfig})).json();
+  const endpoint = `${API_URL}artists/${id}/songs?sort=popularity&page=${page}&per_page=48&access_token=${API_KEY}`;
+  return await (await fetch(endpoint)).json();
  },
 
  fetchSongs: async(id) => {
-  const endpoint = `${API_URL}songs/${id}`;
-  return await (await fetch(endpoint, {...defaultConfig})).json();
+  const endpoint = `${API_URL}songs/${id}?access_token=${API_KEY}`;
+  return await (await fetch(endpoint)).json();
 
  },
 
  fetchSearch: async(searchTerm) => {
-  const endpoint = `${API_SEARCH_URL}?q=${searchTerm}`;
-  return await (await fetch(endpoint, {...defaultConfig})).json();
+  const endpoint = `${API_SEARCH_URL}?q=${searchTerm}&access_token=${API_KEY}`;
+  return await (await fetch(endpoint)).json();
 
  },
 };
