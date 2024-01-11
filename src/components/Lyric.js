b5f5f9d9c1715b32156c3d108e93d655972f9ecd
Oluwasanya Olaoluwa <vantageola4@gmail.com>
2024-01-11 14:26:24 +0100

    hotfix-proxy-url

--- src/components/Lyric.js
+++ src/components/Lyric.js
@@ -11,7 +11,7 @@ const Lyric = ({ link }) => {
     const fetchData = async () => {
         
       try {
-        const proxyUrl = 'https://thingproxy.freeboard.io/fetch/'; // proxy url
+        const proxyUrl = 'https://corsproxy.io/?'; // proxy url
         const response = await axios.get(`${proxyUrl}${link}`);
         const data = response.data;
 
